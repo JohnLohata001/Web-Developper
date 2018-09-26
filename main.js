b95b70c185e83b6e038771e3b92a9d05f3c36d92
JohnLohata001 <37670263+JohnLohata001@users.noreply.github.com>
2018-09-26 15:56:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let showMenu = document.getElementById('nav-manu');
+    if (showmenu.style.display === "block") {
+        showMenu.style.display = "none";
+    } else {
+        showMenu.style.display = "block";
+    }
+}
